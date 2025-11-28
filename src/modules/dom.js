ac5595f7f486ff9d937009e4aa710c20992a912d
TonyLi0916 <tonyli4505@gmail.com>
2025-11-28 03:41:46 -0500

    Add .gitattributes to hide generated css code

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -5,7 +5,7 @@ const results = document.querySelector("#results-container");
 export const renderWeather = (data) => {
   results.innerHTML = `
     <div class="text-indigo-300 flex flex-col items-center gap-3">
-      <h2 class="text-xl font-bold gap-2">${data.city} - ${data.timeZone}</h2>
+      <h2 class="text-3xl text-rose-400 font-extrabold gap-2">${data.city} - ${data.timeZone}</h2>
       <p class="font-science-gothic text-pink-300"> 🌡️ The Current Weather Is: </p> 
       <p class="text-3xl font-bold">${data.temp}° Celsius</p>
       <p>${data.desc}</p>  
